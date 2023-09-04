62b9b861e2128278302598263c193b491e185f60
Kristi <5507151kr@gmail.com>
2023-09-04 20:05:24 +0200

    Autorization

--- src/screens/Login/main.js
+++ src/screens/Login/main.js
@@ -1,62 +1,65 @@
-import { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import React, { useEffect }  from 'react'
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
 import { Keyboard } from 'react-native';
 import AuthStore from '../../stores/auth'
-import {  REQUEST_STATUS } from '../../services/constants'
+import { REQUEST_STATUS } from '../../services/constants'
 import { getSession } from '../../services/auth'
+import * as AppleAuthentication from 'expo-apple-authentication';
+import * as Google from 'expo-auth-session/providers/google';
 
 export const LoginScreen = ({ navigation }) => {
+    const [userInfo, setUserInfo] = React.useState(null);
+    const GOOGLE_CLIENT_ID = '834107509512-4ml4fiue0sovdee82fuj67900vglpsdc.apps.googleusercontent.com';
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        iosClientId:
+          "834107509512-4ml4fiue0sovdee82fuj67900vglpsdc.apps.googleusercontent.com",
+        expoClientId:
+          "834107509512-jlm87hnbatouh2cl7oocv3unu3sfk2qo.apps.googleusercontent.com",
+      });
 
-    const [number, setNumbet] = useState('');
-
-    const setNumber = async() => {
-        const status = await AuthStore.getPin(number)
-        if (status ===  REQUEST_STATUS.success) {
-            navigation.navigate('CodeScreen')
-        }
-    }
+    // const openSettings = async () => {
+    //     navigation.navigate('CreateUserScreen')
+    // }
 
     useEffect(() => {
-        getSession().then((session) => {
-            if (session && session.token) {
-                navigation.navigate('CouponScreen')
+        console.log("response: ", response);
+      }, [response]);
+
+    const handleAppleSignIn = async () => {
+        try {
+            const credential = await AppleAuthentication.signInAsync({
+                requestedScopes: [AppleAuthentication.AppleAuthenticationScope.EMAIL, AppleAuthentication.AppleAuthenticationScope.FULL_NAME],
+            });
+            // Handle the credential (e.g., send it to your server for authentication)
+            // openSettings()
+            console.log(credential);
+        } catch (error) {
+            if (error.code === 'ERR_CANCELED') {
+                // Handle user cancellation
+            } else {
+                // Handle other errors
             }
-        })
-    }, []);
+        }
+    };
 
     return (
-        <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            accessible={false}>
-            <View style={styles.container}>
-                <Image
-                    source={require('../../../assets/logo.png')}
-                    style={styles.imageStyle}
-                />
-                <Text style={styles.textStyle}>Регистрация</Text>
-                <View style={styles.phoneBlock}>
-                    <TextInput style={styles.codeTextInputStyle}
-                        placeholder='+375'
-                        placeholderTextColor={'white'}
-                        editable={false} />
-                    <TextInput style={styles.phoneTextInputStyle}
-                        onChangeText={setNumbet}
-                        value={number}
-                        keyboardType='numeric'
-                        placeholder="Номер телефона"
-                        maxLength={9}
-                        placeholderTextColor={'grey'} />
+        <View style={styles.container}>
+            <Image source={require('../../../assets/logo.png')} style={styles.imageStyle} />
+            <Text style={styles.textStyle}>Регистрация</Text>
+            <AppleAuthentication.AppleAuthenticationButton
+                buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+                buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.WHITE}
+                cornerRadius={5}
+                style={styles.button}
+                onPress={handleAppleSignIn} />
+            <TouchableHighlight style={styles.googleButton} color={'black'} title="Sign In with Google" onPress={() => promptAsync({ useProxy: false, showInRecents: true })}>
+                <View style={styles.containerForGoogleButton}>
+                    <Image source={require('../../../assets/google.png')} style={styles.googleImage} />
+                    <Text style={styles.buttonText}>Sign In with Google</Text>
                 </View>
-                <TouchableOpacity
-                    style={[styles.buttonStyle, { opacity: number.length == 9 ? 1 : 0.3 }]}
-                    disabled={number.length == 9 ? false : true}
-                    onPress={setNumber}
-                >
-                    <Text style={styles.buttonText}>Вход</Text>
-                </TouchableOpacity>
-            </View>
-        </TouchableWithoutFeedback>
-    )
+            </TouchableHighlight>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -64,53 +67,43 @@ const styles = StyleSheet.create({
         width: 120,
         height: 120,
     },
+    googleImage: {
+        width: 12,
+        height: 12,
+    },
     container: {
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
         backgroundColor: 'black',
-        paddingTop: 80
+        paddingTop: 80,
+        gap: 20
     },
     textStyle: {
         color: 'white',
-        fontSize: 30,
-        paddingBottom: 10,
-        paddingTop: 30
+        fontSize: 40,
+        paddingBottom: 200,
     },
-    phoneBlock: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingBottom: 10,
+    button: {
+        width: 200,
+        height: 40,
     },
-    codeTextInputStyle: {
-        color: 'white',
-        width: '20%',
-        height: 50,
-        borderWidth: 0.5,
-        borderColor: 'white',
-        borderRadius: 8,
-        margin: 15,
-        textAlign: 'center',
-    },
-    phoneTextInputStyle: {
-        color: 'white',
-        width: '60%',
-        height: 50,
-        padding: 10,
-        borderWidth: 0.5,
-        borderColor: 'white',
-        borderRadius: 8
-    },
-    buttonStyle: {
-        width: '40%',
-        paddingVertical: 10,
-        borderRadius: 8,
-        backgroundColor: '#57A167',
-        opacity: 0.3,
-        alignItems: 'center'
+    googleButton: {
+        backgroundColor: 'white',
+        width: 200,
+        height: 40,
+        borderRadius: 5
     },
     buttonText: {
-        fontSize: 20,
-        color: '#fff'
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '500',
+        paddingLeft: 5
+    },
+    containerForGoogleButton: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 })
