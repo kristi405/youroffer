6d5a2dcfc2290fa5dce3bfd3fd6904976965fc1f
Kristi <5507151kr@gmail.com>
2023-10-22 21:07:36 +0200

    Build 1.0

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 import { Main } from './src/main'
 import * as NavigationBar from 'expo-navigation-bar';
+import 'expo-dev-client';
 
 export default function App() {
   if (Platform.OS == 'android') {
