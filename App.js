7bacc88b7d2e0d2f00d13d9c6371d91e1ff16d4d
Andrei Zhuk <a.zhuk>
2024-02-24 01:07:10 +0100

    fix

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { Platform } from 'react-native';
+import { useEffect } from 'react';
 import { Main } from './src/main'
 import * as NavigationBar from 'expo-navigation-bar';
 import 'expo-dev-client';
 import * as Sentry from 'sentry-expo';
+import * as Updates from 'expo-updates';
 
 Sentry.init({
   dsn: 'https://803698e536aa4528ac38b38788093389@app.glitchtip.com/5267',
@@ -14,6 +16,23 @@ export default function App() {
   if (Platform.OS == 'android') {
     NavigationBar.setBackgroundColorAsync("black");
   }
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Ошибка при загрузке обновления: ${error}`);
+    }
+  }
+
+  useEffect(() => { onFetchUpdateAsync() }, []);
+
   return (
     <Main />
   );
