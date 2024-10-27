0bdf9378d865e54b3ac820adaec0f5381f9ebc6f
Andrei Zhuk <a.zhuk>
2024-10-27 21:27:37 +0100

    fix

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,8 +3,8 @@ export const REQUEST_STATUS = {
     error: 'error'
 }
 
-export const FILE_URL = `https://dev.myoffer.life/api/v1/file/`;
-export const API_URL = 'https://dev.myoffer.life/';
+export const FILE_URL = `https://myoffer.life/api/v1/file/`;
+export const API_URL = 'https://myoffer.life/';
 export const ANDROID_CLIENT_ID = '431628664212-ncgb1pcdupvjm1o2h9ahqm55birluvsh.apps.googleusercontent.com'
 export const IOS_CLIENT_ID = '834107509512-4ml4fiue0sovdee82fuj67900vglpsdc.apps.googleusercontent.com'
 
